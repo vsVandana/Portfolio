04bd9b8128fcd477659ff670f78fe30cd023bea8
vsvandana <vandananawhall@gmail.com>
2023-03-19 10:54:29 +0530

    add home page with download pdf using anchor tag

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 
+$(document).ready(function(){
+    // FOR ANIMATION HEADING
+    var typed = new Typed('.element',{
+        strings:['Vandana Nawhal', 'a Web Developer','a Web Designer'],
+        smartBackspace: true,
+        typeSpeed:100,
+        backSpeed: 100,
+        loop:true,
+        loopCount:Infinity,
+        startDelay:1000,
+    });
+
+// Download CV
+
+
+
+
+
+
+
+
+
+
+
+})
