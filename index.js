fc2def59cd7ccf985718bf3887ee4193d70b1077
vsvandana <vandananawhall@gmail.com>
2024-01-09 13:55:28 +0530

    REMOVE SOME BUGS

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 // Download CV
     $("#downloadCV").click(function (e) {
         e.preventDefault();          
-        window.open("resume.pdf", "_blank")
+        window.open("Vandana Sharma's resume.pdf", "_blank")
     });
 
     // ADD DARK BACKGROUND AT SCROLL
@@ -29,6 +29,11 @@ $(document).ready(function(){
         }
     }
 
+    $(document).ready(function () {
+        $('.navbar-toggler').on('click', ()=>{
+            $('.navbar-collapse').toggleClass('show');
+        })
+    })
 
 
 
